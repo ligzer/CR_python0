6ca178360d5bf4a9f0452e061e6b3463edcc8efc
Mike Pro <mvp@mallina.su>
2021-12-13 17:58:13 +0300

    Наивное решение

--- ProxyHandler.py
+++ ProxyHandler.py
@@ -1,13 +1,18 @@
 from http.server import BaseHTTPRequestHandler
 from http import HTTPStatus
-from http.client import HTTPConnection, HTTPSConnection
+from http.client import HTTPSConnection
 import shutil
 
 
 class ProxyHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        con = HTTPSConnection('news.ycombinator.com')
+        con.request("GET", self.path)
+        response = con.getresponse()
         self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        shutil.copyfileobj(response, self.wfile)
 
     def do_POST(self):
         self.send_response(HTTPStatus.OK)
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from http.server import HTTPServer
+from http.server import HTTPServer, ThreadingHTTPServer
 from ProxyHandler import ProxyHandler
 
 
-def run(server_class=HTTPServer, handler_class=ProxyHandler):
+def run(server_class=ThreadingHTTPServer, handler_class=ProxyHandler):
     server_address = ('', 8080)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
