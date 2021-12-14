0786acb0f3fd52c44f1056286af1751ea6f368b9
Mike Pro <mvp@mallina.su>
2021-12-14 19:49:22 +0300

    skip in-html javascript blocks

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ import re
 
 def __add_tm_to_element__(element):
     """Add ™-symbol to 6-letter words in lxml element recursively"""
+    if element.tag == 'script':
+        return  # Ignore in-html scripts content
     if element.text:
         element.text = re.sub(r'\b(\w{6})\b', r'\1™', element.text)  # Add ™-symbol
     for child in element.getchildren():
