12321a2c6b91c966474de5737a5f20f5ba129ad2
Mike Pro <mvp@mallina.su>
2021-12-14 21:21:55 +0300

    Some headers are proxied too - to use cache

--- ProxyHandler.py
+++ ProxyHandler.py
@@ -10,18 +10,40 @@ class ProxyHandler(BaseHTTPRequestHandler):
 
     Proxy request to news.ycombinator.com and add tm-symbol to 6-words
     """
+    ACCEPTABLE_RESPONSE_HEADERS = [
+        'Cache-Control',
+        'Content-Type',
+        # 'Content-Encoding', TODO: Decide what to do with different encodings 4exmpl gzip
+        'Vary',
+        'Referrer-Policy',
+        'Cookie',
+    ]
+
+    ACCEPTABLE_REQUEST_HEADERS = [
+        'Cache-Control',
+        'Accept',
+        'User-Agent',
+        # 'Accept-Encoding', # TODO: Decide what to do with different encodings 4exmpl gzip
+        'Accept-Language',
+        'Cookie',
+    ]
 
     def do_GET(self):
         con = HTTPSConnection('news.ycombinator.com')
-        con.request(self.command, self.path)
+        headers = {k: v for k, v in self.headers.items() if k in self.ACCEPTABLE_REQUEST_HEADERS}
+        con.request(self.command, self.path, headers=headers)
         response = con.getresponse()
-        self.send_response(HTTPStatus.OK)
+        self.send_response(response.status)
+        for k,v in response.headers.items():
+            if k in self.ACCEPTABLE_RESPONSE_HEADERS:
+                self.send_header(k, v)
         self.end_headers()
-        if response.headers.get('Content-Type', None) == 'text/html; charset=utf-8':
-            # For html-files add tm-symbol
-            data = add_tm(response)
-            self.wfile.write(data)
-        else:
-            # For others send original answer
-            shutil.copyfileobj(response, self.wfile)
+        if response.status == HTTPStatus.OK:
+            if response.headers.get('Content-Type', None) == 'text/html; charset=utf-8':
+                # For html-files add tm-symbol
+                data = add_tm(response)
+                self.wfile.write(data)
+            else:
+                # For others send original answer
+                shutil.copyfileobj(response, self.wfile)
         con.close()
