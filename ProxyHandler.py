19d530e48492239a87355e16158e48b86aa57887
Mike Pro <mvp@mallina.su>
2021-12-15 14:50:03 +0300

    Add support for POST-requests

--- ProxyHandler.py
+++ ProxyHandler.py
@@ -16,11 +16,14 @@ class ProxyHandler(BaseHTTPRequestHandler):
         # 'Content-Encoding', TODO: Decide what to do with different encodings 4exmpl gzip
         'Vary',
         'Referrer-Policy',
-        'Cookie',
+        'Set-Cookie',
+        'Location',
     ]
 
     ACCEPTABLE_REQUEST_HEADERS = [
         'Cache-Control',
+        'Content-Type',
+        'Content-Length',
         'Accept',
         'User-Agent',
         # 'Accept-Encoding', # TODO: Decide what to do with different encodings 4exmpl gzip
@@ -47,3 +50,28 @@ class ProxyHandler(BaseHTTPRequestHandler):
                 # For others send original answer
                 shutil.copyfileobj(response, self.wfile)
         con.close()
+
+
+    def do_POST(self):
+        con = HTTPSConnection('news.ycombinator.com')
+        headers = {k: v for k, v in self.headers.items() if k in self.ACCEPTABLE_REQUEST_HEADERS}
+        body_str = self.rfile.read(int(self.headers.get('Content-Length', '0')))
+        con.request(self.command, self.path, body=body_str, headers=headers)
+        response = con.getresponse()
+        self.send_response(response.status)
+        print(response.headers)
+        for k,v in response.headers.items():
+            if k in self.ACCEPTABLE_RESPONSE_HEADERS:
+                self.send_header(k, v)
+        self.end_headers()
+
+        if response.status == HTTPStatus.OK:
+            if response.headers.get('Content-Type', None) == 'text/html; charset=utf-8':
+                # For html-files add tm-symbol
+                data = rebuild_page(response)
+                self.wfile.write(data)
+            else:
+                # For others send original answer
+                shutil.copyfileobj(response, self.wfile)
+        con.close()
+
