bd3be679d740edef618c0efa78b46d101f352a59
Mike Pro <mvp@mallina.su>
2021-12-15 19:27:04 +0300

    fix replacing urls

--- tests.py
+++ tests.py
@@ -10,7 +10,8 @@ import lxml.html
 
 
 class ProxyTest(unittest.TestCase):
-
+    """Test for correct proxying"""
+    # TODO: Setup testing server and tests for 304 code, post, cookies etc..
     def setUp(self) -> None:
         """Empty Test"""
 
@@ -19,6 +20,7 @@ class ProxyTest(unittest.TestCase):
 
 
 class RegExpTest(unittest.TestCase):
+    """Tests for correct detecting words"""
 
     def test_5_6_7_length_words(self):
         elem_a = lxml.html.fromstring('<div><b>abcdef1</b><b>ab12ef</b><b>ab3de</b></div>')
@@ -37,29 +39,41 @@ class RegExpTest(unittest.TestCase):
             b'<b>a-e-f1&#8482; abcdef-10 abcdef-abcdef ten-10&#8482; abcde_ ab_dc1&#8482;</b>',
             lxml.html.tostring(elem_a))
 
-        elem_b = lxml.html.fromstring('<b>a_1_2b</b>')
+        elem_b = lxml.html.fromstring('<b>a_1_2b a____b a_-_df ab--bc abd--bcd</b>')
         __add_tm_to_element__(elem_b)
         self.assertEqual(
-            b'<b>a_1_2b&#8482;</b>',
+            b'<b>a_1_2b&#8482; a____b&#8482; a_-_df ab--bc&#8482; abd--bcd</b>',
             lxml.html.tostring(elem_b))
 
+    def test_urls(self):
+        elem_a = lxml.html.fromstring('<a href="httpss://yab.ru">httpss://yab.ru absad ya.rub httpss://yab.ru</a>')
+        __add_tm_to_element__(elem_a)
+        self.assertEqual(b'<a href="httpss://yab.ru">httpss://yab.ru absad ya.rub httpss://yab.ru</a>', lxml.html.tostring(elem_a))
+
+        elem_b = lxml.html.fromstring('<span class="sitestr">github.com/spencertipping</span>')
+        __add_tm_to_element__(elem_a)
+        self.assertEqual(b'<span class="sitestr">github.com/spencertipping</span>', lxml.html.tostring(elem_b))
 
 class UrlReplaceTest(unittest.TestCase):
-
+    """Tests for correct replacing urls"""
     def test_a_https_href(self):
         elem_a = lxml.html.fromstring(
-            '<a href="https://news.ycombinator.com/item?id=13713480">https://news.ycombinator.com/item?id=13713480</a>')
+            '<a href="https://news.ycombinator.com/item?id=13713480">'
+            'https://news.ycombinator.com/item?id=13713480</a>')
         __change_links__(elem_a)
         self.assertEqual(
-            b'<a href="https://localhost:8080/item?id=13713480">https://localhost:8080/item?id=13713480</a>',
+            b'<a href="http://localhost:8080/item?id=13713480">'
+            b'http://localhost:8080/item?id=13713480</a>',
             lxml.html.tostring(elem_a))
 
     def test_a_http_href(self):
         elem_a = lxml.html.fromstring(
-            '<a href="http://news.ycombinator.com/item?id=13713480">http://news.ycombinator.com/item?id=13713480</a>')
+            '<a href="http://news.ycombinator.com/item?id=13713480">'
+            'http://news.ycombinator.com/item?id=13713480</a>')
         __change_links__(elem_a)
         self.assertEqual(
-            b'<a href="http://localhost:8080/item?id=13713480">http://localhost:8080/item?id=13713480</a>',
+            b'<a href="http://localhost:8080/item?id=13713480">'
+            b'http://localhost:8080/item?id=13713480</a>',
             lxml.html.tostring(elem_a))
 
 
--- utils.py
+++ utils.py
@@ -1,6 +1,42 @@
 import lxml.html
-from typing import TextIO
+from typing import TextIO, List
 import re
+
+__regexp_url__ = re.compile(r'(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.][a-z]{2,4}\/)'
+                            r'(?:[^\s\(\)<>]+|\((?:[^\s\(\)<>]+|(?:\([^\s\(\)<>]+\)))*\))+'
+                            r'(?:\((?:[^\s\(\)<>]+|(?:\([^\s\(\)<>]+\)))*\)|[^\s`!\(\)\[\]{};:\'".,<>?«»“”‘’]))')
+
+__regexp_domain__ = re.compile(r'([\w-]+(?:\.[\w-]+)+)')
+__regexp_dash_underline__ = re.compile(r'(-_+|_+-)')
+__regexp_6letter_word__ = re.compile(r'(?i)(?<![\w-])([a-z][\w-]{4}[a-z0-9])(?![\w-])')
+
+
+def __split_to_words__(text: str) -> List:
+    """Split text with rexexp's recursively"""
+
+    # Split by any url-like sequence
+    # Example: anyprotocol://yaras.ru/asddas?asd=1&asd=dsa
+    splited1 = enumerate(__regexp_url__.split(text))
+    for i, t1 in splited1:
+        if i % 2 == 0:
+            # Split by any domain-like sequence
+            # Example: domasd.ads.d.sd.d.ru
+            splited2 = enumerate(__regexp_domain__.split(t1))
+            for j, t2 in splited2:
+                if j % 2 == 0:
+                    # Split by any dash-underline sequence
+                    # Example: __-
+                    splited3 = enumerate(__regexp_dash_underline__.split(t2))
+                    for z, t3 in splited3:
+                        if z % 2 == 0:
+                            yield 'value', t3
+                        else:
+                            yield 'splitter', t3
+                else:
+                    yield 'splitter', t2
+        else:
+            yield 'splitter', t1
+
 
 
 def __add_tm_to_element__(element: lxml.html.Element) -> None:
@@ -8,10 +44,14 @@ def __add_tm_to_element__(element: lxml.html.Element) -> None:
     if element.tag == 'script':
         return  # Ignore in-html scripts content
     if element.text:
-        # Add ™-symbol
-        # TODO: fix matching words like a_-_-c
-        element.text = re.sub(r'([^a-zA-Z-]|^)([a-zA-Z][a-zA-Z0-9_-]{4}[a-zA-Z0-9])([^a-zA-Z0-9_-]|$)', r'\1\2™\3',
-                              element.text)
+        text = ''
+        for k, v in __split_to_words__(element.text):
+            if k == 'value':
+                # Add ™-symbol to words
+                text += __regexp_6letter_word__.sub(r'\1™', v)
+            else:
+                text += v
+        element.text = text
     for child in element.getchildren():
         __add_tm_to_element__(child)
 
@@ -22,7 +62,7 @@ def __change_links__(element: lxml.html.Element) -> None:
         url = el.get(par, None)
         # TODO: Fix for urls with multiple :// sequences
         if url.startswith('http') and url.find('://news.ycombinator.com') != -1:
-            el.set(par, url.replace('http://news.ycombinator.com', 'http://localhost:8080'))
+            url = url.replace('http://news.ycombinator.com', 'http://localhost:8080')
             el.set(par, url.replace('https://news.ycombinator.com', 'http://localhost:8080'))
         if el.text:
             el.text = el.text.replace('http://news.ycombinator.com', 'http://localhost:8080')
@@ -37,4 +77,4 @@ def rebuild_page(inp_file: TextIO) -> str:
     page = lxml.html.parse(inp_file, parser=utf8_parser)
     __add_tm_to_element__(page.getroot())
     __change_links__(page.getroot())
-    return lxml.html.tostring(page, pretty_print=True)
+    return lxml.html.tostring(page, pretty_print=False)
